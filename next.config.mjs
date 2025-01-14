2f6a43994f32b19d870d05608f5a2efc3522d5b4
savvanna dhanaraj sai prasanth <saiprasanth0012@gmail.com>
2025-01-14 21:13:52 +0530

    updated next.config.mjs to solve errors with searchparams

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['deccan-furniture.s3.ap-south-2.amazonaws.com'],
       },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+      },
 };
 
 export default nextConfig;
