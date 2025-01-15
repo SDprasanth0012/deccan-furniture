7db02527d47538ddd25429e9e725ad21f79ce4be
savvanna dhanaraj sai prasanth <saiprasanth0012@gmail.com>
2025-01-15 08:58:08 +0530

    updated thank-you

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,19 +1,35 @@
-// pages/thank-you.js
+"use client"
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const ThankYou = () => {
-    return (
-      <div className="flex flex-col items-center justify-center mt-64 bg-[#f4f0ea] text-[#4d3d30] text-center">
-        <h1 className="text-4xl font-bold mb-4">Thank You!</h1>
+  const router = useRouter();
+
+  useEffect(() => {
+    // Set a timeout to redirect after 2 seconds
+    const timer = setTimeout(() => {
+      router.push("/profile");
+    }, 2000); // 2000 ms = 2 seconds
+
+    // Cleanup the timeout if the component is unmounted
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <div className="h-screen overflow-y-hidden flex justify-center items-center">
+      <div className="flex flex-col items-center justify-center  bg-[#f4f0ea] text-[#4d3d30] text-center">
+        <h1 className="text-4xl font-normal
+         mb-4">Thank You!</h1>
         <p className="text-lg mb-8">Your order has been placed successfully.</p>
-        <a 
-          href="/products" 
+        <a
+          href="/products"
           className="bg-[#4d3d30] text-[#f4f0ea] border border-[#4d3d30] rounded px-4 py-2 hover:bg-[#f4f0ea] hover:text-[#4d3d30] transition-colors duration-300"
         >
-          continue shopping 
+          Continue shopping
         </a>
       </div>
-    );
-  };
-  
-  export default ThankYou;
-  
+    </div>
+  );
+};
+
+export default ThankYou;
